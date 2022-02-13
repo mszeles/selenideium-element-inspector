d27d4d60aed9d990bb299f33ef7cee9f93bab6c0
MSzeles <miklos.szeles@dmgmori-heitec.hu>
2022-02-13 12:27:18 +0100

    Logging name based selector in case if it exists to console.

--- src/content.js
+++ src/content.js
@@ -6,8 +6,24 @@ document.onclick = function(e){
     if (target.hasAttribute("id")) {
         console.log("$(By.id('" + target.getAttribute("id") + "'))")
     }
+   if (target.hasAttribute("name")) {
+        var elementsByName = document.getElementsByName(target.getAttribute("name"));
+        if (hasOnlyOne(elementsByName)) {
+            console.log("$(By.name('" + target.getAttribute("name") + "'))")
+        }
+    }
     console.log("Selenium")
     if (target.hasAttribute("id")) {
         console.log("driver.findElement(By.id('" + target.getAttribute("id") + "'))")
     }
+    if (target.hasAttribute("name")) {
+        var elementsByName = document.getElementsByName(target.getAttribute("name"));
+        if (hasOnlyOne(elementsByName)) {
+            console.log("driver.findElement(By.name('" + target.getAttribute("name") + "'))")
+        }
+    }
+}
+
+function hasOnlyOne(elementList) {
+    return elementList.length == 1;
 }
