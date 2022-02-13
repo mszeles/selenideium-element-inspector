c3c57e98c8cbf1bd54886ecce35acb8ffd0b9955
MSzeles <miklos.szeles@dmgmori-heitec.hu>
2022-02-13 16:25:12 +0100

    Logging CSS based selector in case if it exists to console.

--- src/content.js
+++ src/content.js
@@ -29,6 +29,8 @@ document.onclick = function(e){
     if (hasOnlyOneWithText(allElements, target.text)) {
         console.log("$(withText('" + target.text + "'))")
     }
+    console.log("$('" + getCssSelector(target) + "'))")
+
     console.log("Selenium")
     if (target.hasAttribute("id")) {
         console.log("driver.findElement(By.id('" + target.getAttribute("id") + "'))")
@@ -47,6 +49,7 @@ document.onclick = function(e){
             console.log("driver.findElement(By.linkText('" + target.text + "'))")
         }
     }
+    console.log("driver.findElement(By.cssSelector('" + getCssSelector(target) + "'))")
 }
 
 function hasOnlyOne(elementList) {
@@ -66,3 +69,31 @@ function hasOnlyOneWithText(elementList, text) {
     }
     return foundCount == 1
 }
+
+// Source: https://stackoverflow.com/questions/3620116/get-css-path-from-dom-element
+function getCssSelector(e) {
+    console.log(e)
+    if (!(e instanceof Element)) {
+        return;
+    }
+    var path = [];
+    while (e.nodeType === Node.ELEMENT_NODE) {
+        var selector = e.nodeName.toLowerCase();
+        if (e.id) {
+            selector += '#' + e.id;
+            path.unshift(selector);
+            break;
+        } else {
+            var sib = e, nth = 1;
+            while (sib = sib.previousElementSibling) {
+                if (sib.nodeName.toLowerCase() == selector)
+                   nth++;
+            }
+            if (nth != 1)
+                selector += ":nth-of-type("+nth+")";
+        }
+        path.unshift(selector);
+        e = e.parentNode;
+    }
+    return path.join(" > ");
+ }
