bbcc2bb4d7b712ec0b428168fdb172d523b0453c
MSzeles <miklos.szeles@dmgmori-heitec.hu>
2022-02-17 13:38:39 +0100

    Implemented "Generate CSS selectors based on any unique attribute" #4

--- src/content.js
+++ src/content.js
@@ -1,52 +1,73 @@
-document.onclick = function(e){
+window.onclick = function(e){
     e = e || window.event;
     var target = e.target || e.srcElement
     console.log(target)
-    console.log("Selenide")
+    var selenideSelectors = []
+    var seleniumSelectors = []
     if (target.hasAttribute("id")) {
-        console.log("$(By.id('" + target.getAttribute("id") + "'));")
+        selenideSelectors.push(createSelenideSelector("By.id('" + target.getAttribute("id") + "')"))
+        seleniumSelectors.push(createSeleniumSelector("By.id('" + target.getAttribute("id") + "')"))
     }
-   if (target.hasAttribute("name")) {
-        var elementsByName = document.getElementsByName(target.getAttribute("name"));
+    if (target.hasAttribute("name")) {
+        var elementsByName = document.getElementsByName(target.getAttribute("name"))
         if (hasOnlyOne(elementsByName)) {
-            console.log("$(By.name('" + target.getAttribute("name") + "'));")
+            selenideSelectors.push(createSelenideSelector("By.id('" + target.getAttribute("id") + "')"))
+            seleniumSelectors.push(createSeleniumSelector("By.id('" + target.getAttribute("id") + "')"))
         }
     }
-    var elementsByTagName = document.getElementsByTagName(target.tagName);
+    var elementsByTagName = document.getElementsByTagName(target.tagName)
     if (hasOnlyOne(elementsByTagName)) {
-        console.log("$(By.tagName('" + target.tagName + "'));")
+        selenideSelectors.push(createSelenideSelector("By.tagName('" + target.tagName.toLowerCase() + "')"))
+        seleniumSelectors.push(createSeleniumSelector("By.tagName('" + target.tagName.toLowerCase() + "')"))
     }
-    var links = document.getElementsByTagName("a");
+    var links = document.getElementsByTagName("a")
     if (target.tagName.toLowerCase() == "a") {
         if (hasOnlyOneWithText(links, target.text)) {
-            console.log("$(By.linkText('" + target.text + "'));")
+            selenideSelectors.push(createSelenideSelector("By.linkText('" + target.text + "')"))
+            seleniumSelectors.push(createSeleniumSelector("By.linkText('" + target.text + "')"))
         }
     }
     var allElements = document.getElementsByTagName("*")
     if (hasOnlyOneWithText(allElements, target.text)) {
-        console.log("$(withText('" + target.text + "'));")
+        selenideSelectors.push(createSelenideSelector("withText('" + target.text + "')"))
     }
-    console.log("$('" + getCssSelector(target) + "'));")
+    selenideSelectors.push(createSelenideSelector("'" + getCssSelector(target) + "'"))
+    seleniumSelectors.push(createSeleniumSelector("'" + getCssSelector(target) + "'"))
 
+    collectAttributeBasedSelectors(target, selenideSelectors, seleniumSelectors)
+
+    console.log("Selenide")
+    for (let i = 0; i < selenideSelectors.length; i++) {
+        console.log(selenideSelectors[i])
+    }
     console.log("Selenium")
-    if (target.hasAttribute("id")) {
-        console.log("driver.findElement(By.id('" + target.getAttribute("id") + "'));")
+    for (let i = 0; i < seleniumSelectors.length; i++) {
+        console.log(seleniumSelectors[i])
     }
-    if (target.hasAttribute("name")) {
-        var elementsByName = document.getElementsByName(target.getAttribute("name"));
-        if (hasOnlyOne(elementsByName)) {
-            console.log("driver.findElement(By.name('" + target.getAttribute("name") + "'));")
+}
+
+function collectAttributeBasedSelectors(element, selenideSelectors, seleniumSelectors) {
+    var attributes = element.attributes
+    for (let i = 0; i < attributes.length; i++) {
+        var nodeName = attributes[i].nodeName.toLowerCase()
+        if (nodeName != 'id' && nodeName != 'class' && nodeName != 'name') {
+            var cssSelector = element.tagName.toLowerCase() + "[" + nodeName + " = '" + escape(attributes[i].nodeValue) + "']"
+            console.log(cssSelector)
+            var allElements = document.querySelectorAll(cssSelector)
+            if (hasOnlyOne(allElements)) {
+                selenideSelectors.push(createSelenideSelector("'" + cssSelector + "'"))
+                seleniumSelectors.push(createSeleniumSelector("'" + cssSelector + "'"))
+            }
         }
     }
-    if (hasOnlyOne(elementsByTagName)) {
-        console.log("driver.findElement(By.tagName('" + target.tagName + "'));")
-    }
-    if (target.tagName.toLowerCase() == "a") {
-        if (hasOnlyOneWithText(links, target.text)) {
-            console.log("driver.findElement(By.linkText('" + target.text + "'));")
-        }
-    }
-    console.log("driver.findElement(By.cssSelector('" + getCssSelector(target) + "'));")
+}
+
+function createSelenideSelector(selector) {
+    return "$(" + selector + ");"
+}
+
+function createSeleniumSelector(selector) {
+    return "driver.findElement(" + selector + ");"
 }
 
 function hasOnlyOne(elementList) {
@@ -68,28 +89,30 @@ function hasOnlyOneWithText(elementList, text) {
 }
 
 // Source: https://stackoverflow.com/questions/3620116/get-css-path-from-dom-element
-function getCssSelector(e) {
-    if (!(e instanceof Element)) {
+function getCssSelector(element) {
+    if (!(element instanceof Element)) {
+        console.log("Element is not an Element: " + element)
         return;
     }
     var path = [];
-    while (e.nodeType === Node.ELEMENT_NODE) {
-        var selector = e.nodeName.toLowerCase();
-        if (e.id) {
-            selector += '#' + e.id;
+    while (element.nodeType === Node.ELEMENT_NODE) {
+        var selector = element.nodeName.toLowerCase();
+        if (element.id) {
+            selector += '#' + element.id;
             path.unshift(selector);
             break;
         } else {
-            var sib = e, nth = 1;
-            while (sib = sib.previousElementSibling) {
-                if (sib.nodeName.toLowerCase() == selector)
+            var previousSibling = element, nth = 1;
+            while (previousSibling = previousSibling.previousElementSibling) {
+                if (previousSibling.nodeName.toLowerCase() == selector)
                    nth++;
             }
             if (nth != 1)
                 selector += ":nth-of-type("+nth+")";
         }
         path.unshift(selector);
-        e = e.parentNode;
+        element = element.parentNode;
+
     }
     return path.join(" > ");
  }
